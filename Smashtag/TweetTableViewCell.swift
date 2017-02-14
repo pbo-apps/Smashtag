f379ece36b1f2ddc0ab0200a751c1055891ded01
Pete Bounford <petebounford@gmail.com>
2017-02-14 09:00:29 +0100

    Create new view to display tweets in a custom fashion

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Pete Bounford on 14/02/2017.
+//  Copyright © 2017 PBO Apps. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
